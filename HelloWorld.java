6085cf1fc8dbb216f36eef57bcd6e0e8d0ae00a1
bmoses00 <bmoses00@stuy.edu>
2019-02-28 12:51:13 -0500

    Modified HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,15 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        try {
-            System.out.println("Hello, " + args[0] + "!");
-        }
-        catch (Exception e){
-            System.out.println("Hello World!");
+    	if (Math.random() < .5) {
+    		System.out.println("I do not say hello");
+    	}
+    	else {
+		    try {
+		        System.out.println("Hello, " + args[0] + "!");
+		    }
+		    catch (Exception e){
+		        System.out.println("Hello World!");
+		    }
         }
     }
 }
